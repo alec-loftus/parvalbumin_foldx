b4c77513d306b924e63c714ec0532dc223182cbc
Peter Kekenes-Huskey <pkekeneshuskey@luc.edu>
2025-03-27 13:52:41 -0500

    config added

---
+++ config.bash
@@ -0,0 +1,5 @@
+export FOLDX_BINARY=/home/pkekeneshuskey/sources/foldxLinux64/
+export FOLDX_LOCATION=/home/pkekeneshuskey/sources/foldxLinux64/foldx_20251231
+# this needs to be in local directrory  for now 
+#ln -s /home/pkekeneshuskey/sources/foldxLinux64/rotabase.txt 
+ln -s /home/pkekeneshuskey/source/foldxLinux64/rotabase.txt 
